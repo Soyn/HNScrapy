2ad11c85b86de1d6e81b63f5199a2d87dc21afe8
Ajay Kumar <ajkumar_25@yahoo.com>
2013-12-12 14:47:26 +0530

    minor bugs

--- hn/settings.py
+++ hn/settings.py
@@ -3,10 +3,10 @@ BOT_NAME = 'hn'
 SPIDER_MODULES = ['hn.spiders']
 NEWSPIDER_MODULE = 'hn.spiders'
 
-DATABASE = {'drivername': 'postgres',
-            'username': 'ajay',
-            'password': '12345',
-            'database': 'hn'}
+DATABASE = {'drivername': '',
+            'username': '',
+            'password': '',
+            'database': ''}
 
 ITEM_PIPELINES = {
     'hn.pipelines.HnPipeline':300
